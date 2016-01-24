522daa42545c7ada9f36b50c79251b7ae689f400
Yu,Xianbo <yxbneil@163.com>
2016-01-24 16:37:41 +0800

    first commit

---
+++ public/js/page_app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by hansneil on 18/1/16.
+ */
+var page = angular.module('ownPage', []);
+page.controller('neilOwnPage', ['$scope', '$http', function($scope, $http){
+    $scope.title = "Hansneil";
+    $scope.click = function(){
+        $scope.active = 1;
+    }
+}]);
