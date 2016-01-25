8277ed7f8831e64304aafeb4700a452adbfa6a6c
Yu,Xianbo <yxbneil@163.com>
2016-01-26 00:01:02 +0800

    remove some un-angularjs code & version 1.1.4

--- public/js/page_app.js
+++ public/js/page_app.js
@@ -4,13 +4,53 @@
 var page = angular.module('ownPage', []);
 page.controller('neilOwnPage', ['$scope', '$http', function($scope, $http){
     $scope.title = "Hansneil";
+    $scope.tags_le = [
+        {area: 'Web Design', style: 'web-design'},
+        {area: 'Web Develop', style: 'web-develop'}
+    ];
+    $scope.tags = [
+        {area: 'Flat Design', style: 'flat-design'},
+        {area: 'Photoshop', style: 'photoshop'},
+        {area: 'Illustrate', style: 'illustrate'},
+    ];
+    $scope.progressLeft = [
+        {number: '1', skill: 'HTML5'},
+        {number: '2', skill: 'CSS'},
+        {number: '3', skill: 'SCSS'},
+        {number: '4', skill: 'JavaScript'}
+    ];
+    $scope.progressRight = [
+        {number: '5', skill: 'JQuery'},
+        {number: '6', skill: 'Bootstrap'},
+        {number: '7', skill: 'AngularJS'},
+        {number: '8', skill: 'NodeJs'}
+    ];
+    $scope.webLink = [
+        {name: 'github', link: 'https://github.com/hansneil'},
+        {name: 'weibo', link: 'http://weibo.com/2434892144/profile?rightmod=1&wvr=6&mod=personinfo'},
+        {name: 'linkedin', link: 'http://www.linkedin.com/in/先波-余-8b658695?trk=nav_responsive_tab_profile_pic'},
+        {name: 'google', link: 'https://plus.google.com/109007860879929993270'},
+        {name: 'stackoveflow', link: 'http://stackoverflow.com/users/5835253/hansneil'},
+        {name: 'facebook', link: 'https://www.facebook.com/profile.php?id=100004108532958'}
+    ];
     $scope.click = function(){
         $scope.active = 1;
     };
     /*console.log(echarts.init);*/
 }]);
 page.controller('ProgressController', ['$scope', '$attrs', function($scope, $attrs) {
-
+    /*$scope.progressLeft = [
+        {number: '1', skill: 'HTML5'},
+        {number: '2', skill: 'CSS'},
+        {number: '3', skill: 'SCSS'},
+        {number: '4', skill: 'JavaScript'}
+    ];
+    $scope.progressRight = [
+        {number: '5', skill: 'JQuery'},
+        {number: '6', skill: 'Bootstrap'},
+        {number: '7', skill: 'AngularJS'},
+        {number: '8', skill: 'NodeJs'}
+    ];*/
 }]);
 page.directive('progressGroup', function(){
     return {
@@ -28,10 +68,11 @@ page.directive('progressBar', function() {
         transclude: true,
         replace: true,
         templateUrl: '/tpls/progress.html',
-        scope: {skill: '@', number: '@'},
+        scope: {skill: '=', number: '='},
         link: function(scope, element, attrs, ProgressController) {
-            var num = attrs.number;
-            element.addClass('bar-' + num);
+            /*var num = attrs['number'];
+            console.log(element);
+            element.addClass('bar-' + num);*/
         }
     }
 });
