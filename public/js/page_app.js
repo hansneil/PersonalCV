9a11bd18234aa85bf63a5414ad93050d2e5836f9
Yu,Xianbo <yxbneil@163.com>
2016-01-25 14:25:27 +0800

    my cv page version1.0

--- public/js/page_app.js
+++ public/js/page_app.js
@@ -9,3 +9,29 @@ page.controller('neilOwnPage', ['$scope', '$http', function($scope, $http){
     };
     /*console.log(echarts.init);*/
 }]);
+page.controller('ProgressController', ['$scope', '$attrs', function($scope, $attrs) {
+
+}]);
+page.directive('progressGroup', function(){
+    return {
+        restrict: 'E',
+        controller: 'ProgressController',
+        link: function(scope, element, attrs) {
+            element.addClass('progress');
+        }
+    }
+});
+page.directive('progressBar', function() {
+    return {
+        require: '^progressGroup',
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        templateUrl: '/tpls/progress.html',
+        scope: {skill: '@', number: '@'},
+        link: function(scope, element, attrs, ProgressController) {
+            var num = attrs.number;
+            element.addClass('bar-' + num);
+        }
+    }
+});
